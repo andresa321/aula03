2ed26dbd09459f991942c16f2f02aa5dbd6f262c
andresa321 <andresaofc14@gmail.com>
2025-03-31 14:27:25 -0300

    enviando exercicio 01

---
+++ aula_03.py
@@ -0,0 +1,7 @@
+#codigo para ler o nome de uma pessoa, a sua idade e seu salario no final.
+nome=input("digite seu nome: ")
+idade=int(input("digite sua idade: "))
+salario=float(input("digite seu salario: "))
+print(f"nome: {nome}\n"
+      f"idade: {idade}\n"
+      f"salario: {salario}\n")
