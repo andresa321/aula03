4e37ff6e279361df0660c8c4b9afe44f7f0c52a1
andresa321 <andresaofc14@gmail.com>
2025-03-31 16:17:12 -0300

    exercicio de if/else

---
+++ exercicio_04.py
@@ -0,0 +1,7 @@
+#codigo receber dois numeros do usuario e mostrar ele em ordem crescente.
+num1=int(input(" digite o numero: "))
+num2=int(input(" digite o numero: "))
+if num1 > num2:
+    print(num2, num1)
+else:
+    print(num1, num2)
