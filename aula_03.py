20112e231f7888f0aa8e8fa48c46d7762165d615
andresa321 <andresaofc14@gmail.com>
2025-03-31 14:47:34 -0300

    exercicio salario novo

--- aula_03.py
+++ aula_03.py
@@ -5,3 +5,8 @@ salario=float(input("digite seu salario: "))
 print(f"nome: {nome}\n"
       f"idade: {idade}\n"
       f"salario: {salario}\n")
+percentual=int(input("insira o percentual do aumento: "))
+aumento=(salario/100)* percentual
+
+salarionovo=(salario+aumento)
+print(salarionovo)
