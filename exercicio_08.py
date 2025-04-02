b3b272409fac89f1c65072ac4285092241438810
andresa321 <andresaofc14@gmail.com>
2025-04-02 16:59:53 -0300

    exercicio quebra a cabeça

---
+++ exercicio_08.py
@@ -0,0 +1,11 @@
+#codigo para saber se o numero é par ou impar.
+
+numero=int(input("digite o numero: "))
+
+
+if numero %2 == 0:
+    print(f"o numero sera par.")
+
+else:
+    print(f"ele sera impar.")
+
---
+++ exercicio_09.py
@@ -0,0 +1,17 @@
+#codigo para decifrar a logica e fazer um codigo para executar.
+
+hora1 = int(input("digite a hora"))
+minuto1 = int(input("digite o minuto"))
+
+hora2 = int(input("digite a hora"))
+minuto2 = int(input("digite o minuto"))
+
+somahora = hora1 + hora2
+somaminuto = minuto1 + minuto2
+
+
+
+
+
+
+
